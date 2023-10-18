a9aa5433ada6d2a474aeb96f3be4325b2dad303f
Eduardo <ealvarez@carminaticonsulting.com>
2023-10-18 14:30:30 -0500

    created movie component and successfully called movie api

--- movieDB/src/components/MovieList.jsx
+++ movieDB/src/components/MovieList.jsx
@@ -1,22 +1,32 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { movieApiOptions, MOVIE_API_IMAGES_URL } from "../movieApiOptions";
 
 export const MovieList = () => {
   const [movieList, setMovieList] = useState([]);
-  const url =
-    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
-  const AuthStr = "Bearer ".concat(import.meta.env.VITE_MOVIE_KEY);
   useEffect(() => {
-    axios
-      .get(url, { headers: { Authorization: AuthStr } })
-      .then((response) => {
-        console.log(response);
-        setMovieList(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    fetch(
+      "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc",
+      movieApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => setMovieList(response))
+      .catch((err) => console.error(err));
   }, []);
-  console.log(movieList);
-  return <div>movieList</div>;
+  console.log(movieList.results, "movieList");
+  return (
+    <div>
+      {movieList.results &&
+        movieList.results.map((results, key) => (
+          <div id={key}>
+            <img
+              alt={results.title}
+              src={`${MOVIE_API_IMAGES_URL}${results.poster_path}`}
+            />
+            <label>{results.title}</label>
+            <p>{results.overview}</p>
+          </div>
+        ))}
+    </div>
+  );
 };
---
+++ movieDB/src/movieApiOptions.js
@@ -0,0 +1,14 @@
+export const movieApiOptions = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYTBkZDZmM2QwZmVhODZmMTA1NTIwMTAyOWY1ZTJhOSIsInN1YiI6IjYwYzUyYzFhN2E5N2FiMDA0MmIzN2FhMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1P0qk33FkUAXC_MUFo7MUDMK3ILMO0N77tU1MNQQZnw",
+  },
+};
+
+export const MOVIE_API_URL = "https://api.themoviedb.org";
+
+export const MOVIE_API_KEY = "ca0dd6f3d0fea86f1055201029f5e2a9";
+
+export const MOVIE_API_IMAGES_URL = "https://image.tmdb.org/t/p/w500/";
